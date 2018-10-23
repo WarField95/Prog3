81238d87da47128265cd7c8b201efbf858b56e94
WarField95 <43983038+WarField95@users.noreply.github.com>
2018-10-23 09:24:13 +0200

    Update VocableTrainer.py

--- VocableTrainer.py
+++ VocableTrainer.py
@@ -1,3 +1,9 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import random
 
 
@@ -17,6 +23,9 @@ class VokabelTrainer(object):
 
 
     def hinzufuegen(self):
+        
+        """Input german word for firsts input_newger
+           then input its translation for input_neweng"""
 
         input_newger=input("Pls add new german word here").lower()
         input_neweng=input("Pls add new english word here").lower()
@@ -25,13 +34,16 @@ class VokabelTrainer(object):
 
 
     def trainieren(self, number_questions, wrongs, rights, runs):
+    
+        """for testing if the words are right or wrong
+           tests if key and value typed in are equal"""
 
         for i in range(number_questions):
             key1 = random.choice(list(self.d.keys()))
-            value1 = self.d.get(self, key1)
+            value1 = self.d.get(key1)
             print(value1)
             print(key1)
-            input_voc=input("Pls write vocab for {} here: ".format(value1))
+            input_voc=input("Pls write vocab for {} here: ".format(key1))
 
             if input_voc.lower() == value1.lower():
                 print("right")
@@ -45,6 +57,8 @@ class VokabelTrainer(object):
 
 
     def zuruecksetzen(self, number_questions, rights, wrongs, runs):
+    
+        """sets back the stats for the last run"""
 
         self.rights=0
         self.wrongs=0
@@ -55,6 +69,8 @@ class VokabelTrainer(object):
 
 
     def ergebnisAufgabe(self, wrongs, rights):
+    
+        """prints the answers, how much wrong, how much right"""
 
 
         print("Wrong answers: " + wrongs + "\n Right answers: " + rights)
@@ -64,25 +80,28 @@ class VokabelTrainer(object):
         for keys, values in self.d.items():
             print(keys, end=" : ")
             print(values)
+            
+    
+    #test = VokabelTrainer()
 
 
-    # input_choice=int(input("Pls choose what to do: \n"
-    #                    "1 to add a pair of vocabs \n"
-    #                    "2 to set back your stuff \n"
-    #                    "3 to train your voc knowledge"))
-
-    # while input_choice != 5:
-    #
-    #     if input_choice == 1:
-    #         hinzufuegen()
-    #     elif input_choice == 2:
-    #         zuruecksetzen(rights, wrongs, runs)
-    #     elif input_choice == 3:
-    #         trainieren(d, number_questions, wrongs, rights, runs)
+  #  input_choice=int(input("Pls choose what to do: \n"
+  #                      "1 to add a pair of vocabs \n"
+  #                      "2 to set back your stuff \n"
+ #                       "3 to train your voc knowledge"))
+#
+  #  
+ #   
+#    if input_choice == 1:
+#        test.hinzufuegen()
+#    elif input_choice == 2:
+#        test.zuruecksetzen(rights, wrongs, runs)
+#    elif input_choice == 3:
+#        test.trainieren(1,0,0,1)
 
 
 test = VokabelTrainer()
 test.trainieren(1,0,0,1)
-test.ergebnisAufgabe(wrongs, rights)
+#test.ergebnisAufgabe()
 #test.hinzufuegen()
 #test.printDict()
